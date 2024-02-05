e11e70c6316874334a437e6c9ac5ae4cee90d907
Litrop Simon <52304869@edu.campus02.at>
2024-02-05 19:18:37 +0100

    BMICalculatorTest Klasse, Dependencies und Maven Plugin erstellen

---
+++ src/test/java/BMICalculatorTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @Test
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateBMI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateBMICategory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBMICategoryName() {
+    }
+}
